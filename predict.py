b806928c5399f6e13b31e540f25d2ad3a76bbb42
sgdxbc <sgdxbc@gmail.com>
2021-03-22 15:44:17 +0800

    Add predict
    Signed-off-by: sgdxbc <sgdxbc@gmail.com>

---
+++ predict.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import tensorflow as tf
+from utils import *
+
+data_dir = Path() / "data" / "mini_speech_commands"
+audio_path = data_dir / "right" / "0ab3b47d_nohash_0.wav"
+model_path = Path() / "classifier_model_42"
+
+spectrogram = get_spectrogram(decode_audio(tf.io.read_file(str(audio_path))))
+spectrogram = tf.expand_dims(spectrogram, -1)
+spectrogram = tf.expand_dims(spectrogram, 0)
+model = tf.keras.models.load_model(str(model_path))
+model.summary()
+print(get_commands(data_dir))
+print(model.predict(spectrogram))
--- train.py
+++ train.py
@@ -8,82 +8,73 @@ from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras import layers
 from tensorflow.keras import models
 
+from utils import *
+
 seed = 42
 tf.random.set_seed(seed)
 np.random.seed(seed)
-save_path = pathlib.Path() / 'classifier_model_42'
+save_path = pathlib.Path() / "classifier_model_42"
 
-data_dir = pathlib.Path('data/mini_speech_commands')
+data_dir = pathlib.Path("data/mini_speech_commands")
 if not data_dir.exists():
-  tf.keras.utils.get_file(
-      'mini_speech_commands.zip',
-      origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
-      extract=True,
-      cache_dir='.', cache_subdir='data')
-commands = np.array(tf.io.gfile.listdir(str(data_dir)))
-commands = commands[commands != 'README.md']
-print('Commands:', commands)
-filenames = tf.io.gfile.glob(str(data_dir) + '/*/*')
+    tf.keras.utils.get_file(
+        "mini_speech_commands.zip",
+        origin="http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip",
+        extract=True,
+        cache_dir=".",
+        cache_subdir="data",
+    )
+commands = get_commands(data_dir)
+print("Commands:", commands)
+filenames = tf.io.gfile.glob(str(data_dir) + "/*/*")
 filenames = tf.random.shuffle(filenames)
 num_samples = len(filenames)
-print('Number of total examples:', num_samples)
-print('Number of examples per label:',
-      len(tf.io.gfile.listdir(str(data_dir/commands[0]))))
+print("Number of total examples:", num_samples)
+print(
+    "Number of examples per label:",
+    len(tf.io.gfile.listdir(str(data_dir / commands[0]))),
+)
 
 train_files = filenames[:6400]
-val_files = filenames[6400: 6400 + 800]
+val_files = filenames[6400 : 6400 + 800]
 test_files = filenames[-800:]
 
-print('Training set size', len(train_files))
-print('Validation set size', len(val_files))
-print('Test set size', len(test_files))
+print("Training set size", len(train_files))
+print("Validation set size", len(val_files))
+print("Test set size", len(test_files))
 
-def decode_audio(audio_binary):
-  audio, _ = tf.audio.decode_wav(audio_binary)
-  return tf.squeeze(audio, axis=-1)
 
 def get_label(file_path):
-  parts = tf.strings.split(file_path, os.path.sep)
+    parts = tf.strings.split(file_path, os.path.sep)
 
-  # Note: You'll use indexing here instead of tuple unpacking to enable this 
-  # to work in a TensorFlow graph.
-  return parts[-2] 
+    # Note: You'll use indexing here instead of tuple unpacking to enable this
+    # to work in a TensorFlow graph.
+    return parts[-2]
+
 
 def get_waveform_and_label(file_path):
-  label = get_label(file_path)
-  audio_binary = tf.io.read_file(file_path)
-  waveform = decode_audio(audio_binary)
-  return waveform, label
+    label = get_label(file_path)
+    audio_binary = tf.io.read_file(file_path)
+    waveform = decode_audio(audio_binary)
+    return waveform, label
+
 
 AUTOTUNE = tf.data.AUTOTUNE
 
-def get_spectrogram(waveform):
-  # Padding for files with less than 16000 samples
-  zero_padding = tf.zeros([16000] - tf.shape(waveform), dtype=tf.float32)
-
-  # Concatenate audio with padding so that all audio clips will be of the 
-  # same length
-  waveform = tf.cast(waveform, tf.float32)
-  equal_length = tf.concat([waveform, zero_padding], 0)
-  spectrogram = tf.signal.stft(
-      equal_length, frame_length=255, frame_step=128)
-      
-  spectrogram = tf.abs(spectrogram)
-
-  return spectrogram
 
 def get_spectrogram_and_label_id(audio, label):
-  spectrogram = get_spectrogram(audio)
-  spectrogram = tf.expand_dims(spectrogram, -1)
-  label_id = tf.argmax(label == commands)
-  return spectrogram, label_id
+    spectrogram = get_spectrogram(audio)
+    spectrogram = tf.expand_dims(spectrogram, -1)
+    label_id = tf.argmax(label == commands)
+    return spectrogram, label_id
+
 
 def preprocess_dataset(files):
-  files_ds = tf.data.Dataset.from_tensor_slices(files)
-  output_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
-  output_ds = output_ds.map(
-      get_spectrogram_and_label_id,  num_parallel_calls=AUTOTUNE)
-  return output_ds
+    files_ds = tf.data.Dataset.from_tensor_slices(files)
+    output_ds = files_ds.map(get_waveform_and_label, num_parallel_calls=AUTOTUNE)
+    output_ds = output_ds.map(get_spectrogram_and_label_id, num_parallel_calls=AUTOTUNE)
+    return output_ds
+
 
 spectrogram_ds = train_ds = preprocess_dataset(train_files)
 val_ds = preprocess_dataset(val_files)
@@ -96,38 +87,40 @@ train_ds = train_ds.cache().prefetch(AUTOTUNE)
 val_ds = val_ds.cache().prefetch(AUTOTUNE)
 
 for spectrogram, _ in spectrogram_ds.take(1):
-  input_shape = spectrogram.shape
-print('Input shape:', input_shape)
+    input_shape = spectrogram.shape
+print("Input shape:", input_shape)
 num_labels = len(commands)
 
 norm_layer = preprocessing.Normalization()
 norm_layer.adapt(spectrogram_ds.map(lambda x, _: x))
 
-model = models.Sequential([
-    layers.Input(shape=input_shape),
-    preprocessing.Resizing(32, 32), 
-    norm_layer,
-    layers.Conv2D(32, 3, activation='relu'),
-    layers.Conv2D(64, 3, activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Dropout(0.25),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dropout(0.5),
-    layers.Dense(num_labels),
-])
+model = models.Sequential(
+    [
+        layers.Input(shape=input_shape),
+        preprocessing.Resizing(32, 32),
+        norm_layer,
+        layers.Conv2D(32, 3, activation="relu"),
+        layers.Conv2D(64, 3, activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.25),
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dropout(0.5),
+        layers.Dense(num_labels),
+    ]
+)
 
-print(model.summary())
+model.summary()
 model.compile(
     optimizer=tf.keras.optimizers.Adam(),
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'],
+    metrics=["accuracy"],
 )
 
-EPOCHS = 10
+EPOCHS = 20
 history = model.fit(
-    train_ds, 
-    validation_data=val_ds,  
+    train_ds,
+    validation_data=val_ds,
     epochs=EPOCHS,
     callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
 )
