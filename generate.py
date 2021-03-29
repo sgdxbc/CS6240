23a835d5a3009533e035e601235bd29eacb474aa
sgdxbc <sgdxbc@gmail.com>
2021-03-29 08:50:14 +0800

    Fix previous problems
    Signed-off-by: sgdxbc <sgdxbc@gmail.com>

--- generate.py
+++ generate.py
@@ -19,20 +19,15 @@ model = tf.keras.models.load_model(str(model_path))
 
 audio, sample_rate = tf.audio.decode_wav(tf.io.read_file(str(audio_path)))
 audio = tf.squeeze(audio, -1)
-spectrogram = get_spectrogram(audio)
-spectrogram = tf.expand_dims(spectrogram, -1)
-spectrogram = tf.expand_dims(spectrogram, 0)
 adv = tf.Variable([0.] * MIN_SAMPLE)
 
 def loss_fn():
-    spec = tf.signal.stft(adv, frame_length=255, frame_step=128)
+    mix = adv + audio
+    spec = tf.signal.stft(mix, frame_length=255, frame_step=128)
     spec = tf.abs(spec)
     spec = tf.expand_dims(spec, -1)
     spec = tf.expand_dims(spec, 0)
-    mix = spectrogram + spec
-    pred = model(mix, training=False)[0]
-    pred_min, pred_max = tf.reduce_min(pred), tf.reduce_max(pred)
-    pred = (pred - pred_min) / (pred_max - pred_min)
+    pred = model(spec, training=False)[0]
     dist = tf.norm(pred - target)
     norm = tf.norm(adv)
     return dist + alpha * norm
@@ -40,6 +35,6 @@ def loss_fn():
 losses = tfp.math.minimize(loss_fn, num_steps=1, optimizer=tf.optimizers.Adam(), trainable_variables=[adv])
 with tf.control_dependencies([losses]):
     opt_adv = tf.identity(adv)
-# print(opt_adv[:100])
+print(adv[:100])
 tf.io.write_file(str(adv_path), tf.audio.encode_wav(tf.expand_dims(adv, -1), sample_rate))
 tf.io.write_file(str(mix_path), tf.audio.encode_wav(tf.expand_dims(adv + audio, -1), sample_rate))
