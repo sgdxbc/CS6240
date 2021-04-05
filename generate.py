037aa4ed60223765d2696f9560aa03a5a9664587
sgdxbc <sgdxbc@gmail.com>
2021-04-05 10:53:04 +0800

    Generate with multiple commands per class
    Signed-off-by: sgdxbc <sgdxbc@gmail.com>

--- generate.py
+++ generate.py
@@ -1,22 +1,19 @@
 from pathlib import Path
 import tensorflow as tf
-from tensorflow.keras import losses, optimizers
 from utils import *
 
 # *** Configuration
 # common
 data_dir = Path() / "data" / "mini_speech_commands"
 adv_path = Path() / "right2left.wav"
-mix_name = "right2left(mix)"
 model_path = Path() / "classifier_model_42"
 alpha = 5.0
+train_commands_per_class = 1
+val_commands_per_class = 10
 # flipping
-adv_sample_number = 2000  # ~50ms
-adv_delay_interval = 50  # ~1ms
-target_map = [
-    (data_dir / "right" / "0ab3b47d_nohash_0.wav", "left"),
-    (data_dir / "left" / "0b09edd3_nohash_0.wav", "right"),
-]
+adv_sample_number = 50 * (SAMPLE_RATE // 1000)
+adv_delay_interval = 10 * (SAMPLE_RATE // 1000)
+target_pairs = [("left", "right"), ("right", "left")]
 # *** Configuration End
 
 physical_devices = tf.config.list_physical_devices("GPU")
@@ -24,83 +21,93 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 commands = get_commands(data_dir)
 model = tf.keras.models.load_model(str(model_path))
-audio_target_map = [
-    (decode_audio(tf.io.read_file(str(path))), commands == command)
-    for path, command in target_map
-]
-mix_per_audio = (MIN_SAMPLE - adv_sample_number) // adv_delay_interval
-audio_list, target_list = [], []
-for audio, target in audio_target_map:
-    for i in range(mix_per_audio):
-        audio_list.append(audio)
-        target_list.append(target)
-audio_list, target_list = tf.stack(audio_list), tf.stack(target_list)
-
+mix_per_audio = (SAMPLE_RATE - adv_sample_number) // adv_delay_interval
+target_map = {origin: target == commands for origin, target in target_pairs}
+audio_map = {
+    origin: [
+        decode_audio(tf.io.read_file(filename))
+        for i, filename in enumerate(tf.io.gfile.glob(str(data_dir / origin) + "/*"))
+        if i < train_commands_per_class + val_commands_per_class
+    ]
+    for origin, _ in target_pairs
+}
+# audio_per_class = len(next(iter(audio_map.values())))
+# assert all(len(audio_list) == audio_per_class for audio_list in audio_map.values())
 adv = tf.Variable(tf.random.normal([adv_sample_number]))
 
 
 def pred(mix):
-    spec = tf.signal.stft(mix, frame_length=255, frame_step=128)
-    spec = tf.abs(spec)
-    spec = tf.expand_dims(spec, -1)
-    return model(spec, training=False)
+    return model(tf.expand_dims(extract_features(mix), -1), training=False)
 
 
-def mix():
-    padded = tf.concat(
-        [
-            adv,
-            tf.zeros([MIN_SAMPLE - adv_sample_number]),
-        ],
-        0,
+def single_x_y(origin, index, delay):
+    return (
+        audio_map[origin][index]
+        + tf.roll(
+            tf.concat([adv, tf.zeros([SAMPLE_RATE - adv_sample_number])], 0), delay, 0
+        ),
+        target_map[origin],
     )
-    padded_list = tf.stack(
-        [
-            tf.roll(padded, adv_delay_interval * i, 0)
-            for i in range(mix_per_audio)
-            for _ in range(len(audio_target_map))
-        ]
+
+
+print(
+    f"predict size: {len(target_map) * train_commands_per_class * mix_per_audio}(train) "
+    + f"{len(target_map) * val_commands_per_class * mix_per_audio}(val)"
+)
+
+
+def x_y(training):
+    r = (
+        range(train_commands_per_class)
+        if training
+        else range(
+            train_commands_per_class, train_commands_per_class + val_commands_per_class
+        )
     )
-    return audio_list + padded_list
+    x, y = [], []
+    for origin in target_map:
+        for i in r:
+            for delay_step in range(mix_per_audio):
+                the_x, the_y = single_x_y(origin, i, delay_step * adv_delay_interval)
+                x.append(the_x)
+                y.append(the_y)
+    return tf.stack(x), tf.stack(y)
 
 
-def loss_fn():
-    dist = tf.keras.losses.mse(target_list, pred(mix()))
+def loss_fn(training=True):
+    x, y = x_y(training)
+    dist = tf.keras.losses.mse(y, pred(x))
     norm = tf.keras.losses.mse(tf.zeros([adv_sample_number]), adv)
     return tf.math.reduce_mean(dist) + alpha * norm
 
 
+def accuracy(y, fx):
+    truth, pred = tf.argmax(y, axis=1), tf.argmax(fx, axis=1)
+    return len(truth[truth == pred]) / len(truth)
+
+
 def opt_loop(loss_fn, var):
     opt = tf.keras.optimizers.Adam()
-    prev_loss = None
+    prev_loss = float("inf")
     for i in range(1000000):
         opt.minimize(loss_fn, [var])
         loss = loss_fn()
-        if prev_loss is not None and tf.abs(loss - prev_loss) < 1e-9:
+        if tf.abs(loss - prev_loss) < 1e-9:
             break
         prev_loss = loss
         if i % 100 == 0:
-            print(prev_loss)
+            print(f"i = {i}, loss = {loss.numpy()}")
+            norm = tf.keras.losses.mse(tf.zeros([adv_sample_number]), adv)
+            print(f"norm = {norm.numpy()}")
+            train_x, train_y = x_y(True)
+            val_x, val_y = x_y(False)
+            print(
+                f"acc(train) = {accuracy(train_y, pred(train_x))}, acc(val) = {accuracy(val_y, pred(val_x))}"
+            )
 
 
 opt_loop(loss_fn, adv)
 
-print("previous")
-print(pred(tf.expand_dims(audio, 0))[0])
-print("after")
-print(pred(mix())[:10])
-
-sample_rate = 16000
 tf.io.write_file(
-    str(adv_path), tf.audio.encode_wav(tf.expand_dims(adv, -1), sample_rate)
-)
-tf.io.write_file(
-    str(Path() / (mix_name + "-1.wav")),
-    tf.audio.encode_wav(tf.expand_dims(mix()[mix_per_audio // 2], -1), sample_rate),
-)
-tf.io.write_file(
-    str(Path() / (mix_name + "-2.wav")),
-    tf.audio.encode_wav(
-        tf.expand_dims(mix()[mix_per_audio + mix_per_audio // 2], -1), sample_rate
-    ),
+    str(adv_path), tf.audio.encode_wav(tf.expand_dims(adv, -1), SAMPLE_RATE)
 )
--- utils.py
+++ utils.py
@@ -2,27 +2,29 @@ import tensorflow as tf
 import numpy as np
 
 
+SAMPLE_RATE = 16000
+
+
 def decode_audio(audio_binary):
     audio, sample_rate = tf.audio.decode_wav(audio_binary)
-    assert sample_rate == 16000, f'sample_rate: {sample_rate}'
-    return tf.squeeze(audio, axis=-1)
-
-
-MIN_SAMPLE = 16000
-
-
-def get_spectrogram(waveform):
+    assert sample_rate == SAMPLE_RATE, f"sample_rate: {sample_rate}"
+    waveform = tf.squeeze(audio, axis=-1)
     # Padding for files with less than 16000 samples
-    zero_padding = tf.zeros([MIN_SAMPLE] - tf.shape(waveform), dtype=tf.float32)
+    zero_padding = tf.zeros([SAMPLE_RATE] - tf.shape(waveform), dtype=tf.float32)
 
     # Concatenate audio with padding so that all audio clips will be of the
     # same length
     waveform = tf.cast(waveform, tf.float32)
-    equal_length = tf.concat([waveform, zero_padding], 0)
-    spectrogram = tf.signal.stft(equal_length, frame_length=255, frame_step=128)
+    return tf.concat([waveform, zero_padding], 0)
 
+
+def get_spectrogram(waveform):
+    return extract_features(waveform)
+
+
+def extract_features(waveform):
+    spectrogram = tf.signal.stft(waveform, frame_length=255, frame_step=128)
     spectrogram = tf.abs(spectrogram)
-
     return spectrogram
 
 
