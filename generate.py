aa3d26ea9bca7911d80b330662aa3311789db164
sgdxbc <sgdxbc@gmail.com>
2021-04-09 01:02:22 +0800

    Minor changes for more memory
    Signed-off-by: sgdxbc <sgdxbc@gmail.com>

--- generate.py
+++ generate.py
@@ -12,13 +12,15 @@ val_commands_per_class = 10
 alpha = 1.0
 adv_sample_number = 600 * (SAMPLE_RATE // 1000)
 adv_delay_interval = 10 * (SAMPLE_RATE // 1000)
-delay_sample_number_per_epoch = 1  # linear increase memory
-stop_when_no_progress_in = 1000 // delay_sample_number_per_epoch
+delay_sample_number_per_batch = 6  # linear increase memory
+batch_per_epoch = 100 // delay_sample_number_per_batch
+stop_when_no_progress_in = 20
 adv_path = Path() / "adv.wav"
 # *** Configuration End
 
 physical_devices = tf.config.list_physical_devices("GPU")
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+for i in range(len(physical_devices)):
+    tf.config.experimental.set_memory_growth(physical_devices[i], True)
 
 commands = get_commands(data_dir)
 model = tf.keras.models.load_model(str(model_path))
@@ -45,12 +47,12 @@ for origin, target in target_map:
             val_xs.append(audio)
             val_yi.append(tf.argmax(commands == target))
 
-x_mat = tf.tile(tf.stack(xs), [delay_sample_number_per_epoch, 1])
-y_mat = tf.tile(tf.stack(ys), [delay_sample_number_per_epoch, 1])
+x_mat = tf.tile(tf.stack(xs), [delay_sample_number_per_batch, 1])
+y_mat = tf.tile(tf.stack(ys), [delay_sample_number_per_batch, 1])
 val_x_mat = tf.stack(val_xs)
 val_yi = tf.convert_to_tensor(val_yi)
 
-delays_of_this_epoch = None
+delays_of_this_batch = None
 
 
 def mix(x_mat):
@@ -61,10 +63,10 @@ def mix(x_mat):
                 shift=delay,
                 axis=0,
             )
-            for delay in delays_of_this_epoch
+            for delay in delays_of_this_batch
         ]
     )
-    return x_mat + tf.tile(padded, [x_mat.shape[0] // delays_of_this_epoch.shape[0], 1])
+    return x_mat + tf.tile(padded, [x_mat.shape[0] // delays_of_this_batch.shape[0], 1])
 
 
 def pred(mix):
@@ -87,13 +89,17 @@ opt = tf.keras.optimizers.Adam()
 
 
 def opt_step():
-    global delays_of_this_epoch
-    delays_of_this_epoch = tf.random.uniform(
-        [delay_sample_number_per_epoch],
-        maxval=SAMPLE_RATE - adv_sample_number,
-        dtype=tf.int32,
-    )
-    opt.minimize(loss_fn, [adv])
+    global delays_of_this_batch
+    # for _ in range(batch_per_epoch):
+    #     delays_of_this_batch = tf.random.uniform(
+    #         [delay_sample_number_per_batch],
+    #         maxval=SAMPLE_RATE - adv_sample_number,
+    #         dtype=tf.int32,
+    #     )
+    for i in range(batch_per_epoch):
+        j = delay_sample_number_per_batch
+        delays_of_this_batch = tf.convert_to_tensor([(j * i + k) * adv_delay_interval for k in range(j)])
+        opt.minimize(loss_fn, [adv])
     return loss_fn()
 
 
@@ -114,8 +120,8 @@ def opt_loop():
 
             train_acc_list, val_acc_list = [], []
             for delay_step in range(mix_per_audio):
-                global delays_of_this_epoch
-                delays_of_this_epoch = tf.convert_to_tensor(
+                global delays_of_this_batch
+                delays_of_this_batch = tf.convert_to_tensor(
                     [delay_step * adv_delay_interval]
                 )
                 train_mixed, val_mixed = (
